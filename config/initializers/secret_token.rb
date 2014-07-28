abbdb5f4f05703a4577ab9f36345b504a8f3c7aa
Jyothirmai <jyothirmai.segu@gmail.com>
2014-07-28 15:25:52 +0200

    commiting to proj2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proj2::Application.config.secret_token = 'f300e2c364d29d31955638ed25806ac2f9724f536b912983aa739b4686540a6b4c29ae824e7da078e5a1f52353f14d34bc36fa0cbb032ddcdaea4a5a209ae9a0'
